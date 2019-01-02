4ac18879d1a254ad446cbc102a5079f4585628e4
Florian Preinstorfer <nblock@archlinux.us>
2019-01-02 20:44:27 +0100

    Create project

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="beancount-nblock",
+    version="0.0.1.dev0",
+    description="A collection of beancount plugins",
+    author="Florian Preinstorfer",
+    author_email="nblock@archlinux.us",
+    url="https://nblock.org",
+    packages=["beancount_nblock"],
+    install_requires=["beancount"],
+    extras_require={
+        "style": ["black", "flake8", "isort"],
+        "test": ["pytest", "pytest-cov", "pytest-randomly", "pytest-xdist"],
+    },
+)
