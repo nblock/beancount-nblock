3526bf8af92c1434c03558b2330888ec8fb9ed95
Florian Preinstorfer <nblock@archlinux.us>
2019-01-02 20:43:50 +0100

    Add unlinked_documents plugin

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # beancount-nblock
 A collection of beancount plugins.
+
+## unlinked_documents: find missing links between documents and transactions
+It might be desireable to enforce a link between certain kind of documents and
+a corresponding transaction. Typical use cases are incoming and outgoing
+invoices where a receipt (referenced via a document directive) should have a
+matching transaction.
+
+The following is valid, according to this plugin:
+
+```
+2019-01-01 * "Payee" "A description" ^ER-some-id
+  Assets:Bank:Checking     300.00 EUR
+  Expenses:Home
+
+2019-01-01 document Assets:Bank:Checking "/path/to/receipt.pdf" ^ER-some-id
+```
+
+An error is generated in case either of the above directives is missing.
+
+### Usage
+Add the following to your beancount file:
+```
+plugin "beancount_nblock.unlinked_documents"
+```
+
+The default list of patterns is: `AR-*`, `ER-*`. A custom list of patterns may
+be configured via:
+```
+plugin "beancount_nblock.unlinked_documents" "PATTERN-FOO-*,PATTERN-BAR-*
+```
---
+++ beancount_nblock/unlinked_documents.py
@@ -0,0 +1,63 @@
+import collections
+import fnmatch
+
+from beancount.core import data
+
+__plugins__ = ["unlinked_documents"]
+
+
+UnlinkedDocumentError = collections.namedtuple(
+    "UnlinkedDocumentError", ["source", "message", "entry"]
+)
+
+
+# Default list of patterns a link should start with to be considered.
+DEFAULT_PATTERNS = ["AR-*", "ER-*"]
+
+
+def parse_patterns(config_str):
+    return [x.strip() for x in config_str.split(",")]
+
+
+def generate_error(link, values, description):
+    try:
+        single = values[0]
+        return UnlinkedDocumentError(
+            single.meta, f"Missing {description} for link {link!r}", single
+        )
+    except IndexError:
+        pass
+
+
+def check_for_errors(cached):
+    errors = []
+
+    for key, values in cached.items():
+        has_transaction = any(filter(lambda v: isinstance(v, data.Transaction), values))
+        if not has_transaction:
+            errors.append(generate_error(key, values, "transaction"))
+
+        has_document = any(filter(lambda v: isinstance(v, data.Document), values))
+        if not has_document:
+            errors.append(generate_error(key, values, "document"))
+
+    return errors
+
+
+def unlinked_documents(entries, options_map, config_str=None):
+    cache = collections.defaultdict(list)
+
+    patterns = DEFAULT_PATTERNS
+    if config_str is not None:
+        patterns = parse_patterns(config_str)
+
+    for entry in entries:
+        if not isinstance(entry, (data.Document, data.Transaction)):
+            continue
+
+        for link in entry.links:
+            for pattern in patterns:
+                if fnmatch.fnmatch(link, pattern):
+                    cache[link].append(entry)
+
+    return (entries, check_for_errors(cache))
