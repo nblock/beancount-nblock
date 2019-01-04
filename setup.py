77379191d5d2d0a89c84b090596881dbfe183879
Florian Preinstorfer <nblock@archlinux.us>
2019-01-04 17:25:42 +0100

    Add README and update URL

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import setup
 
+with open("README.md") as f:
+    long_description = f.read()
+
 setup(
     name="beancount-nblock",
     version="0.0.1",
     description="A collection of beancount plugins",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Florian Preinstorfer",
     author_email="nblock@archlinux.us",
-    url="https://nblock.org",
+    url="https://github.com/nblock/beancount-nblock",
     packages=["beancount_nblock"],
     install_requires=["beancount"],
     extras_require={
