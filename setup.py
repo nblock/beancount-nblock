88653e89150d85da24136490b4e7b8c4aba519de
Florian Preinstorfer <nblock@archlinux.us>
2019-01-04 17:26:00 +0100

    Release 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="beancount-nblock",
-    version="0.0.1",
+    version="0.0.2",
     description="A collection of beancount plugins",
     long_description=long_description,
     long_description_content_type="text/markdown",
